008e16cb5a341981ec91d269aa5f583e68a0ad5d
Your Name <you@example.com>
2023-10-01 23:39:20 +0330

    first commit

---
+++ mysite/myapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    file = models.FileField(upload_to='uploads')
+    
+    def __str__(self) -> str:
+        return str(self.name) 
+
+class OrderDetail(models.Model):
+    customer_email = models.EmailField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    stripe_payment_intent = models.CharField(max_length=200)
+    has_paid = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)  
+      
+   
---
+++ mysite/myapp/views.py
@@ -0,0 +1,74 @@
+from audioop import reverse
+from django.http import JsonResponse,HttpResponseNotFound
+from django.shortcuts import get_object_or_404, render, reverse
+from .models import Product, OrderDetail
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+import stripe, json
+# Create your views here.
+
+
+def index(request):
+    products = Product.objects.all()
+    
+    return render(request, 'myapp/index.html', {'products': products})
+
+
+def detail(request, id):
+    
+    product = Product.objects.get(id=id)
+    stripe_publishable_key = settings.STRIPE_PUBLISHABLE_KEY
+    return render(request, 'myapp/detail.html',{'product': product, 'stripe_publishable_key': stripe_publishable_key})
+    
+@csrf_exempt 
+def create_checkout_session(request, id):
+    request_data= json.loads(request.body)
+    product = Product.objects.get(id=id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        
+        customer_email = request_data['email'],
+        payment_method_types = ['card'],
+        line_items = [
+            {
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data':  {
+                        'name': product.name,
+                    },
+                    'unit_amount': int(product.price * 100)
+                             },
+                'quantity': 1,
+            },
+        ],
+        mode = 'payment',
+        success_url = request.build_absolute_url(reverse('success')) + 
+        "?session_id=(CHECKOUT_SESSION_ID)",
+        cancel_url = request.build_absolute_url(reverse('failed'))
+    )
+    
+    
+    order = OrderDetail()
+    order.customer_email = request_data['email']
+    order.product = product
+    order.stripe_payment_intent = checkout_session['payment_intent']
+    order.amount = int(product.price)
+    order.save()
+    
+    return JsonResponse({'sessionId': checkout_session.id})
+
+def payment_success_view(request):
+    session_id = request.GET.get('session_id')
+    if session_id is None:
+        return HttpResponseNotFound()
+    
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.retrieve(session_id)
+    order = get_object_or_404(OrderDetail, stripe_payment_intent = session.payment_intent)
+    order.has_paid = True
+    order.save()
+    
+    return render(request, 'myapp/payment_success.html',{'order': order})
+
+def payment_failed_view(request):
+    return render(request, 'myapp/failed.html')
